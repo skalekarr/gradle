7e96827f8f612942e553c8e840797c4c71cdc387
Daniel Geiselman <daniel.geiselman@mutualofomaha.com>
2018-03-01 15:21:19 -0600

    3/1/18 - TBD4-410 - TBD4-585 - TBD4-588 - TBD4-637 Fix errors in root-routes

--- client-app/src/js/app/root-routes.jsx
+++ client-app/src/js/app/root-routes.jsx
@@ -3,13 +3,13 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 
 import Header from '../header/header';
 import Footer from '../footer/footer';
-import PolicyLookup from "../PolicyLookup/PolicyLookup";
+import PolicyLookup from '../PolicyLookup/PolicyLookup';
 
 const RootRoutes = () => (
   <div>
     <Route path="/" component={Header} />
     <Switch>
-       <Route path="/" component={PolicyLookup} /> 
+      <Route path="/" component={PolicyLookup} />
       <Redirect from="/" to="/PolicyLookup" />
     </Switch>
     <Route path="/" component={Footer} />
